3bae97e2fc13ae0680c4bf4345276fe0f865f6fd
gokul bs <gokul.b@cybosol.in>
2017-11-12 10:34:57 +0530

    pages

--- src/components/timeline/timeline.module.ts
+++ src/components/timeline/timeline.module.ts
@@ -1,25 +1,25 @@
-import { IonicModule } from 'ionic-angular';
-import { NgModule } from '@angular/core';
-import {
-    TimelineComponent,
-    TimelineItemComponent,
-    TimelineTimeComponent
-} from './timeline';
+// import { IonicModule } from 'ionic-angular';
+// import { NgModule } from '@angular/core';
+// import {
+//     TimelineComponent,
+//     TimelineItemComponent,
+//     TimelineTimeComponent
+// } from './timeline';
 
-export const components = [
-];
+// export const components = [
+// ];
 
-@NgModule({
-  declarations: [
-    TimelineComponent,
-    TimelineItemComponent,
-    TimelineTimeComponent,
-  ],
-  imports: [IonicModule],
-  exports: [
-    TimelineComponent,
-    TimelineItemComponent,
-    TimelineTimeComponent,
-  ]
-})
-export class TimelineComponentModule {}
+// @NgModule({
+//     declarations: [
+//         TimelineComponent,
+//         TimelineItemComponent,
+//         TimelineTimeComponent,
+//     ],
+//     imports: [IonicModule],
+//     exports: [
+//         TimelineComponent,
+//         TimelineItemComponent,
+//         TimelineTimeComponent,
+//     ]
+// })
+// export class TimelineComponentModule { }
